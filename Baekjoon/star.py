8c4397fb815eeef6560721db0287ae4049d969ad
kyeah01 <kyeah01@gmail.com>
2019-01-09 18:01:30 +0900

    D08 | 190109 PM | method2

--- Baekjoon/star.py
+++ Baekjoon/star.py
@@ -1,13 +1,2 @@
 def star(n):
-    T = '*'
-    M = '*  *'
-    B = '*****'
-    return T,M,B
-
-
-P1 = T.center(1,' ')
-P2 = M.center(1,' ')
-P3 = B.center(1,' ')
-print(P1)
-print(P2)
-print(P3)
+    
---
+++ Baekjoon/star2.py
@@ -0,0 +1,7 @@
+def star(n):
+    if n == 1:
+        A = '*'.center(5)
+        B = '* *'.center(5)
+        C = '*****'.center(5)
+        
+
