aa86c2c77b161046c5d94a1d9f762ab36b1a8c41
kang yewon <kyeah01@gmail.com>
2019-02-18 21:53:13 +0900

    20190218 | SWEA solving

--- Problems/SWEA/python_basic/05_STACK2/02_maze.py
+++ Problems/SWEA/python_basic/05_STACK2/02_maze.py
@@ -6,46 +6,44 @@ sys.stdin = open('02_maze_input.txt')
 def search(matrix):
     for n in range(len(matrix)):
         try:
-            print(n, matrix[n].index('2'))
-            return n, matrix[n].index('2')
+            return n, matrix[n].index(2)
         except ValueError:
             continue
 
+def check(n,m,l):
+    return True if 0 <= m <= l and 0 <= n  <= l else False
+
 def back_tracking(n,m):
-    global matrix
-    l = len(matrix)-1
-    if matrix[n-1][m] == 2:
-        return 1
-    elif matrix[n+1][m] == 2:
-        return 1
-    elif matrix[n][m-1] == 2:
-        return 1
-    elif matrix[n][m+1] == 2:
-        return 1
-
-    if matrix[n-1][m] == 0:
-        matrix[n][m] = 4
-        return back_tracking(n-1, m)
-    elif matrix[n+1][m] == 0:
-        matrix[n][m] = 4
-        return back_tracking(n+1, m)
-    elif matrix[n][m-1] == 0:
-        matrix[n][m] = 4
-        return back_tracking(n, m-1)
-    elif matrix[n][m+1] == 0:
-        matrix[n][m] = 4
-        return back_tracking(n, m+1)
-    else:
-        return 0
-
-
+    global matrix, result,N
+    l = N-1
+    matrix[n][m] = 4
+    if check(n-1,m,l) and matrix[n-1][m] == 3:
+        result = 1
+        return
+    if check(n+1,m,l) and matrix[n+1][m] == 3:
+        result = 1
+        return
+    if check(n,m-1,l) and matrix[n][m-1] == 3:
+        result = 1
+        return
+    if check(n,m+1,l) and matrix[n][m+1] == 3:
+        result = 1                                      
+        return
+    if check(n-1,m,l) and matrix[n-1][m] == 0:
+        back_tracking(n-1, m)
+    if check(n+1,m,l) and matrix[n+1][m] == 0:
+        back_tracking(n+1, m)
+    if check(n,m-1,l) and matrix[n][m-1] == 0:
+        back_tracking(n, m-1)
+    if check(n,m+1,l) and matrix[n][m+1] == 0:
+        back_tracking(n, m+1)
 
 for tc in range(1, int(input())+1):
     # 미로 행렬 인풋받기
     N = int(input())
-    matrix = [input() for _ in range(N)]
-    print(matrix)
+    matrix = [list(map(int, input())) for _ in range(N)]
     # 전지역에서 검색해서 2를 일단 찾아야 함.
     ans = search(matrix)
-    print(ans)
-    print(back_tracking(ans[0], ans[1]))
+    result = 0
+    back_tracking(ans[0], ans[1])
+    print(result)
