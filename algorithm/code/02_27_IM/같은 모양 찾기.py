51dd67fefe094e1a8cc1056cc4e2a69bd33af0fe
kangyewon <kyeah01@gmail.com>
2019-02-28 16:17:51 +0900

    190228 | before_im

--- algorithm/code/02_27_IM/같은 모양 찾기.py
+++ algorithm/code/02_27_IM/같은 모양 찾기.py
@@ -13,3 +13,16 @@ for i in range(M-P+1):
         else:
             cnt += 1
 print(cnt)
+
+# 플래그 사용해서 문제풀기
+# cnt = 0
+# for i in range(M-P+1):
+#     for j in range(M-P+1):
+#         flag = 0
+#         for x in range(P):
+#             if mo[i+x][j:j+P] != pattern[x]:
+#                 flag = 1
+#                 break
+#         if flag != 1:
+#             cnt += 1
+# print(cnt)
